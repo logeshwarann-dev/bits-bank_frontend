e52decbb21afbe8d05834a5f103a32e8437e832c
Logeshwaran N <logeshwaran082@gmail.com>
2025-02-01 17:58:54 +0530

    Implement Siderbar with icons

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,12 +1,13 @@
 
 import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({accounts=[], totalBanks, totalCurrentBalance
 }: TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/* DoughnutChart*/}
+            <DoughnutChart accounts={accounts}/>
         </div>
         <div className='flex flex-col gap-6'>
             <h2 className='header-2'>
@@ -16,9 +17,9 @@ const TotalBalanceBox = ({accounts=[], totalBanks, totalCurrentBalance
                 <p className='total-balance-label'>
                     Total Current Balance
                 </p>
-                <p className='total-balance-amount flex-center gap-2'>
+                <div className='total-balance-amount flex-center gap-2'>
                     <AnimatedCounter amount={totalCurrentBalance}/>
-                </p>
+                </div>
 
             </div>
         </div>
