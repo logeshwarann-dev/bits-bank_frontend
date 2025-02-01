148d669ad526b96369c64e493df3a0744bab35b2
Logeshwaran-N <logeshwaran082@gmail.com>
2025-02-01 12:09:19 +0530

    added header and balance box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+
+  const loggedIn = { firstName: "Logesh"  };
   return (
     <section className='home'>
       <div className='home-content'>
         <header className='home-header'>
-          Welcome, Logesh
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox  
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.35}
+          />
         </header>
+
       </div>
     </section>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -66,9 +66,10 @@ export const formatDateTime = (dateString: Date) => {
 };
 
 export function formatAmount(amount: number): string {
-  const formatter = new Intl.NumberFormat("en-US", {
+  const formatter = new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
+    currencyDisplay: "symbol",
     minimumFractionDigits: 2,
   });
 
