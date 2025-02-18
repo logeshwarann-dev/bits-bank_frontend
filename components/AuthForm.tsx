aceb197b1227f6cf92c34852d2a3c6b82e01e4c8
Logeshwaran N <logeshwaran082@gmail.com>
2025-02-18 12:28:37 +0530

    updated home page

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,9 +21,13 @@ import { Input } from "@/components/ui/input"
 import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 
 const AuthForm = ({ type }: { type: string }) => {
+
+    const router = useRouter();
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
@@ -38,12 +42,33 @@ const AuthForm = ({ type }: { type: string }) => {
     })
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
+    const onSubmit = async (data: z.infer<typeof formSchema>) => {
         setIsLoading(true)
-        console.log(values)
-        setIsLoading(false)
+        
+        try{
+            // sign up with backend & create plaid token
+
+            if (type === 'sign-up'){
+                const newUser = await signUp(data);
+                setUser(newUser);
+
+            }
+
+            if (type == 'sign-in'){
+                const response = await signIn({
+                    email: data.email,
+                    password: data.password,
+                })
+
+                if(response) router.push('/')
+
+            }
+        }catch(error){
+            console.log(error)
+        } finally{
+            
+            setIsLoading(false)
+        }
     }
 
     return (
@@ -93,6 +118,9 @@ const AuthForm = ({ type }: { type: string }) => {
                                     </div>
                                     <CustomInput
                                         control={form.control} name="address1" label="Address" placeholder="Enter your specific address" type="text"
+                                    />
+                                    <CustomInput
+                                        control={form.control} name="city" label="City" placeholder="Enter your city" type="text"
                                     />
                                     <div className='flex gap-4'>
                                         <CustomInput
---
+++ lib/actions/user.actions.tsx
@@ -0,0 +1,19 @@
+'use server';
+
+export const signIn = async() => {
+    try{
+        // Mutation / DB / Make fetch
+    } catch(error) {
+        console.error('Error', error);
+
+    }
+}
+
+export const signUp = async(userData: SignUpParams) => {
+    try{
+        // Mutation / DB / Make fetch
+    } catch(error) {
+        console.error('Error', error);
+
+    }
+}
