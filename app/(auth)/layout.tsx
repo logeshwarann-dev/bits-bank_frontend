21318924b45d6380049ec928224fc24493aa817c
Logeshwaran N <logeshwaran082@gmail.com>
2025-04-20 11:43:50 +0530

    configured Amplify & home img

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
           {children}
           <div className="auth-asset">
             <div>
-              <Image src="/icons/auth-image.svg" alt="Auth image" width={500} height={500}/>
+              <Image src="/icons/auth-img.png" alt="Auth image" width={650} height={900}/>
             </div>
             
           </div>
